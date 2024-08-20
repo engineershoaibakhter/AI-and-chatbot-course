002aadc5763a57a30da8ac9270e17edcff5167eb
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-08-20 12:59:41 +0500

    refactor: change color

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <h1 className='bg-[#087808] text-center font-bold text-6xl py-8 text-white'>CV Template</h1>
+    <h1 className='bg-black text-center font-bold text-6xl py-8 text-white'>CV Template</h1>
   )
 }
 
