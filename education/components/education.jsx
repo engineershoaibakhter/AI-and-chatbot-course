e8e4cbba4773011260b4462b50b781e174da4e30
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-09-07 10:47:59 +0500

    "Refactor Education component: updated imports, added edit mode, and removed todo list functionality"

--- education/components/education.jsx
+++ education/components/education.jsx
@@ -1,84 +1,85 @@
-import { addProjectLink, updateProjectLink, fetchProjectLink } from "@/redux/educationSlice";
-import React, { useState } from "react";
-import toast from "react-hot-toast";
-import { MdDelete } from "react-icons/md";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { MdDelete, MdEdit } from "react-icons/md";
+import {
+  fetchProjectLink,
+  updateProjectLink,
+  removeTodo,
+  selectAddLinkData,
+} from "@/redux/educationSlice";
 
 const Education = () => {
   const dispatch = useDispatch();
-  const [showRemove, setShowRemove] = useState(false);
+  const addLink = useSelector(selectAddLinkData);
+  const [editMode, setEditMode] = useState(false);
+  const [currentLink, setCurrentLink] = useState("");
+  const [editingLinkId, setEditingLinkId] = useState("");
 
-  // const { todoList } = useSelector((state) => state?.todo);
+  const handleEditLink = (linkId, link) => {
+    setEditMode(true);
+    setCurrentLink(link);
+    setEditingLinkId(linkId);
+  };
+
+  const handleUpdateLink = () => {
+    if (currentLink === "") {
+      toast.error("Link cannot be empty");
+      return;
+    }
+
+    dispatch(updateProjectLink({ uid: editingLinkId, link: currentLink }))
+      .unwrap()
+      .then(() => {
+        toast.success("Link updated");
+        setEditMode(false);
+        setCurrentLink("");
+        setEditingLinkId("");
+      })
+      .catch((err) => {
+        toast.error("Error updating link");
+      });
+  };
+
+  const handleDeleteLink = (linkId) => {
+    dispatch(removeTodo(linkId))
+      .unwrap()
+      .then(() => {
+        toast.success("Link deleted successfully");
+      })
+      .catch((err) => {
+        toast.error("Error deleting link");
+      });
+  };
+
   return (
     <div className="flex flex-col gap-4 mt-2 p-4 border border-gray-600 rounded-md">
-      {/* {todoList?.length > 0 && ( */}
-        <>
-          <ul
-            className="max-h-[300px] border border-slate-500 p-2 shadow-lg
-            shadow-gray-800 flex flex-col gap-2 overflow-y-auto"
-          >
-            {/* {todoList?.map((item) => ( */}
-              <li
-                className="border-l-green-500 border-green-900 w-full font-medium
-                border-[1px] border-l-[6px] px-2 py-1 cursor-pointer flex items-center justify-between"
-                // key={item?._id}
-              >
-                {/* {item?.todo} */}
-                <MdDelete
-                  onClick={() => {
-                    // dispatch(deleteTodo(item?._id));
-                    toast.success("Todo Deleted Successfully!");
-                  }}
-                  className="text-lg hover:text-red-500 duration-200"
-                />
-              </li>
-            {/* ))} */}
-          </ul>
-          <button
-            onClick={() => setShowRemove(true)}
-            className="text-sm text-orange-500 text-center bg-transparent border-[1px]
-            border-gray-600 rounded-md px-4 py-3 mt-2 hover:border-red-500 duration-200"
-          >
-            Remove Todo
+      {addLink?.length > 0 ? (
+        <ul className="border border-slate-500 p-2 shadow-lg flex flex-col gap-2 overflow-y-auto">
+          {addLink?.map((item) => (
+            <li key={item?.linkId} className="border-l-green-500 border-green-900 w-full flex items-center justify-between">
+              <span>{item?.link}</span>
+              <div className="flex items-center gap-2">
+                <MdEdit onClick={() => handleEditLink(item.linkId, item.link)} className="text-lg cursor-pointer" />
+                <MdDelete onClick={() => handleDeleteLink(item.linkId)} className="text-lg cursor-pointer" />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center text-base text-yellow-600">Your Todo list is empty!</p>
+      )}
+      {editMode && (
+        <div className="flex flex-col gap-2">
+          <input
+            className="border border-gray-600 p-2"
+            type="text"
+            value={currentLink}
+            onChange={(e) => setCurrentLink(e.target.value)}
+          />
+          <button onClick={handleUpdateLink} className="bg-green-400 text-black px-4 py-2 rounded-md">
+            Update Link
           </button>
-        </>
-      {/* )} */}
-      {/* {todoList?.length === 0 && ( */}
-        <p className="text-center text-base text-yellow-600 font-medium tracking-wide">
-          Your Todo list is Empty!
-        </p>
-      {/* )} */}
-      {showRemove && (
-        <div className="fixed inset-0 flex items-center justify-center bg-bodyColor bg-opacity-60 z-50">
-          <div className="relative p-8 bg-black shadow-todoShadow rounded-md max-w-sm w-full mx-4">
-            <p className="text-base lg:text-xl text-center font-medium text-red-500">
-              Are you sure to{" "}
-              <span className="font-semibold underline underline-offset-2 decoration-[1px]">
-                remove
-              </span>{" "}
-              all the todos?
-            </p>
-            <div className="flex items-center justify-center gap-4 mt-4">
-              <button
-                onClick={() => setShowRemove(false)}
-                className="px-6 py-2 text-base font-semibold bg-transparent border-[1px]
-                border-gray-500 hover:border-red-500 duration-300 rounded-md"
-              >
-                No
-              </button>
-              <button
-                onClick={() => {
-                  dispatch(removeTodo());
-                  toast.success("All Todos Removed Successfully!");
-                  setShowRemove(false);
-                }}
-                className="px-6 py-2 text-base font-semibold bg-transparent border-[1px]
-                border-gray-500 hover:border-red-500 duration-300 rounded-md"
-              >
-                Yes
-              </button>
-            </div>
-          </div>
         </div>
       )}
     </div>
