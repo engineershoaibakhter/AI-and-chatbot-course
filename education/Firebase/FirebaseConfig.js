90efecc93b0eade5ebbe40d13b91db4ca56112d2
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-09-07 16:25:57 +0500

    remove analytics

--- education/Firebase/FirebaseConfig.js
+++ education/Firebase/FirebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; // Correct import for Firestore
 
 const firebaseConfig = {
@@ -13,19 +12,9 @@ const firebaseConfig = {
   measurementId: "G-7Z1YEYTM6W"
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCqboyMB-OaXnrfWIoDZdHtPZ7_y9uI7iU",
-//   authDomain: "builders-493ff.firebaseapp.com",
-//   projectId: "builders-493ff",
-//   storageBucket: "builders-493ff.appspot.com",
-//   messagingSenderId: "1044279640882",
-//   appId: "1:1044279640882:web:71acd850ef63fd13338d2b",
-//   measurementId: "G-STLX7X2Y66"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app); // Correct function call
 
 export { db };
